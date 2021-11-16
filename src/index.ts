3b4960d94c020feb8429f27a5b20f719399dc103
janstaffa <agenttupesy@gmail.com>
2021-11-16 13:58:12 +0100

    fixed major bugs in previous commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import ParserClass from './parser';
-import TokenizerClass from './tokenizer';
+import ParserClass from "./parser";
+import TokenizerClass from "./tokenizer";
 const Tokenizer = new TokenizerClass();
 const Parser = new ParserClass();
 export { Parser, Tokenizer };
@@ -7,4 +7,4 @@ export { Parser, Tokenizer };
 // const exp = '2-(x+2(x-5))';
 // console.log(exp, Tokenizer.tokenize(exp));
 
-// console.log(Parser.parse(exp));
+console.log(Parser.parse("sin(x)"));
--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,19 @@
-import { Tokenizer } from '.';
+import { Tokenizer } from ".";
 
 class Parser {
-  private ASSOC: { [key: string]: 'left' | 'right' } = {
-    '^': 'right',
-    '*': 'left',
-    '/': 'left',
-    '+': 'left',
-    '-': 'left',
+  private ASSOC: { [key: string]: "left" | "right" } = {
+    "^": "right",
+    "*": "left",
+    "/": "left",
+    "+": "left",
+    "-": "left",
   };
   private PREC: { [key: string]: number } = {
-    '^': 4,
-    '*': 3,
-    '/': 3,
-    '+': 2,
-    '-': 2,
+    "^": 4,
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2,
   };
 
   /**
@@ -26,7 +26,7 @@ class Parser {
     return 0;
   };
 
-  parse = (expression: string, format: ('rpn' | 'ast') | undefined = 'rpn') => {
+  parse = (expression: string, format: ("rpn" | "ast") | undefined = "rpn") => {
     const tokens = Tokenizer.tokenize(expression);
 
     // RPN
@@ -37,56 +37,55 @@ class Parser {
       const { type, value } = t;
       if (/^(Literal|Var)$/.test(type)) {
         outQueue.push(t);
-      } else if (type === 'Function') {
+      } else if (type === "Function") {
         opStack.push(t);
-      } else if (type === 'ArgumentSeparator') {
-        while (opStack[opStack.length - 1]?.type !== 'LParen') {
+      } else if (type === "ArgumentSeparator") {
+        while (opStack[opStack.length - 1]?.type !== "LParen") {
           outQueue.push(opStack.pop());
         }
-      } else if (type === 'Operator') {
+      } else if (type === "Operator") {
         while (
           opStack[opStack.length - 1] &&
-          opStack[opStack.length - 1].type === 'Operator'
+          opStack[opStack.length - 1].type === "Operator"
         ) {
           const o = opStack[opStack.length - 1];
           const tAssoc = this.ASSOC[value];
           const tPrec = this.PREC[value];
           const oPrec = this.PREC[o.value];
-          if (tPrec < oPrec || (tAssoc === 'left' && tPrec === oPrec)) {
+          if (tPrec < oPrec || (tAssoc === "left" && tPrec === oPrec)) {
             outQueue.push(opStack.pop());
           } else {
             break;
           }
         }
         opStack.push(t);
-      } else if (type === 'LParen') {
+      } else if (type === "LParen") {
         opStack.push(t);
-      } else if (type === 'RParen') {
+      } else if (type === "RParen") {
         while (
           opStack[opStack.length - 1] &&
-          opStack[opStack.length - 1].type !== 'LParen'
+          opStack[opStack.length - 1].type !== "LParen"
         ) {
           outQueue.push(opStack.pop());
         }
         opStack.pop();
-      }
-
-      const lastOp = opStack[opStack.length - 1];
-      if (lastOp && lastOp.type === 'Function') {
-        outQueue.push(opStack.pop());
+        const lastOp = opStack[opStack.length - 1];
+        if (lastOp && lastOp.type === "Function") {
+          outQueue.push(opStack.pop());
+        }
       }
     }
     while (opStack.length > 0) {
       const op = opStack.pop();
       if (!op || /^(LParen|RParen)$/.test(op.type)) {
-        throw new Error('Invalid syntax.');
+        throw new Error("Invalid syntax.");
       }
       outQueue.push(op);
     }
 
-    let output = '';
-    if (format === 'rpn') {
-      output = outQueue.map((o) => o?.value).join('');
+    let output = "";
+    if (format === "rpn") {
+      output = outQueue.map((o) => o?.value).join("");
     }
     return output;
   };
