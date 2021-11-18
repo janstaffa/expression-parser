869c65b836f1516bc1103fb78491d470d6760ef2
Jan Šfaffa <agenttupesy@gmail.com>
2021-11-18 23:42:21 +0100

    added converting of ! to fac() fn

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-import ParserClass from "./parser";
-import TokenizerClass from "./tokenizer";
+import ParserClass from './parser';
+import TokenizerClass from './tokenizer';
 const Tokenizer = new TokenizerClass();
 const Parser = new ParserClass();
 export { Parser, Tokenizer };
 
 // TODO:
-// check for function argument length in tokenizer
-// convert sqrt(x) to root(x, 2) in tokenizer
 // make the entire codebase less trash
+// add factorial
 
-// |x-2| === abs(x-2)
-// |2|x-2| - 1|
-// |(2|x-2| - 1)|
-
-console.log(Tokenizer.tokenize("x + max(10, 20)"));
+console.log(Tokenizer.tokenize('(x-100000)!'));
