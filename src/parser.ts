4e5e7571635c2301466861eef17b227c64742ddc
Jan Šfaffa <agenttupesy@gmail.com>
2021-11-19 20:31:35 +0100

    final touches 2.0

--- src/parser.ts
+++ src/parser.ts
@@ -1,15 +1,15 @@
-import { Tokenizer } from ".";
-import { Token } from "./tokenizer";
+import { Tokenizer } from '.';
+import { Token } from './tokenizer';
 
 class Parser {
   private OPS: {
-    [key: string]: { assoc: "left" | "right"; prec: number };
+    [key: string]: { assoc: 'left' | 'right'; prec: number };
   } = {
-    "^": { assoc: "right", prec: 4 },
-    "*": { assoc: "left", prec: 3 },
-    "/": { assoc: "left", prec: 3 },
-    "+": { assoc: "left", prec: 2 },
-    "-": { assoc: "left", prec: 2 },
+    '^': { assoc: 'right', prec: 4 },
+    '*': { assoc: 'left', prec: 3 },
+    '/': { assoc: 'left', prec: 3 },
+    '+': { assoc: 'left', prec: 2 },
+    '-': { assoc: 'left', prec: 2 },
   };
   private FUNC_ARGS: { [key: string]: number } = {
     sin: 1,
@@ -32,15 +32,21 @@ class Parser {
   evaluate = (
     expression: string,
     unknowns?: { [key: string]: number },
-    flags: { useRadians: boolean } | undefined = { useRadians: false }
+    options?: { useRadians?: boolean; decimals?: number }
   ) => {
+    // set default values
+    options = {
+      useRadians: options?.useRadians || false,
+      decimals: options?.decimals || 4,
+    };
     const parsed = this.parse(expression);
 
+    let i = 0;
     do {
       for (const [idx, token] of parsed.entries()) {
         if (!token) continue;
 
-        if (token.type === "Var") {
+        if (token.type === 'Var') {
           const unknownValue = unknowns?.[token.value];
           if (!unknownValue) {
             throw new Error(
@@ -48,9 +54,9 @@ class Parser {
             );
           }
 
-          parsed[idx] = new Token("Literal", unknownValue.toString());
+          parsed[idx] = new Token('Literal', unknownValue.toString());
           continue;
-        } else if (token.type === "Operator") {
+        } else if (token.type === 'Operator') {
           let result;
           const arg1 = parsed[idx - 2];
           const arg2 = parsed[idx - 1];
@@ -58,26 +64,26 @@ class Parser {
           if (
             !arg1 ||
             !arg2 ||
-            arg1.type !== "Literal" ||
-            arg2.type !== "Literal"
+            arg1.type !== 'Literal' ||
+            arg2.type !== 'Literal'
           ) {
             throw new Error(`Error evaluating expression '${expression}'.`);
           }
 
           switch (token.value) {
-            case "+":
+            case '+':
               result = parseFloat(arg1.value) + parseFloat(arg2.value);
               break;
-            case "-":
+            case '-':
               result = parseFloat(arg1.value) - parseFloat(arg2.value);
               break;
-            case "*":
+            case '*':
               result = parseFloat(arg1.value) * parseFloat(arg2.value);
               break;
-            case "/":
+            case '/':
               result = parseFloat(arg1.value) / parseFloat(arg2.value);
               break;
-            case "^":
+            case '^':
               result = Math.pow(parseFloat(arg1.value), parseFloat(arg2.value));
               break;
           }
@@ -85,9 +91,9 @@ class Parser {
           if (result === undefined)
             throw new Error(`Error evaluating expression '${expression}'.`);
           // replace parameters and operator with result
-          parsed.splice(idx - 2, 3, new Token("Literal", result.toString()));
+          parsed.splice(idx - 2, 3, new Token('Literal', result.toString()));
           break;
-        } else if (token.type === "Function") {
+        } else if (token.type === 'Function') {
           const argCount = this.FUNC_ARGS[token.value];
           if (!argCount) throw new Error(`Unknown function '${token.value}''.`);
           const args = parsed.slice(idx - argCount, idx);
@@ -99,36 +105,36 @@ class Parser {
           let result;
 
           switch (token.value) {
-            case "sin":
-            case "cos":
-            case "tan":
+            case 'sin':
+            case 'cos':
+            case 'tan':
               {
                 const arg = args[0]?.value;
                 if (!arg) break;
-                const realValue = flags.useRadians
+                const realValue = options.useRadians
                   ? parseFloat(arg)
                   : parseFloat(arg) * (Math.PI / 180);
                 switch (token.value) {
-                  case "sin":
+                  case 'sin':
                     result = Math.sin(realValue);
                     break;
-                  case "cos":
+                  case 'cos':
                     result = Math.cos(realValue);
                     break;
-                  case "tan":
+                  case 'tan':
                     result = Math.tan(realValue);
                     break;
                 }
               }
               break;
-            case "sqrt":
+            case 'sqrt':
               {
                 const arg = args[0]?.value;
                 if (!arg) break;
                 result = Math.sqrt(parseFloat(arg));
               }
               break;
-            case "root":
+            case 'root':
               {
                 const arg1 = args[0]?.value;
                 const arg2 = args[1]?.value;
@@ -137,14 +143,14 @@ class Parser {
               }
               break;
 
-            case "abs":
+            case 'abs':
               {
                 const arg = args[0]?.value;
                 if (!arg) break;
                 result = Math.abs(parseFloat(arg));
               }
               break;
-            case "max":
+            case 'max':
               {
                 const arg1 = args[0]?.value;
                 const arg2 = args[1]?.value;
@@ -152,7 +158,7 @@ class Parser {
                 result = Math.max(parseFloat(arg1), parseFloat(arg2));
               }
               break;
-            case "min":
+            case 'min':
               {
                 const arg1 = args[0]?.value;
                 const arg2 = args[1]?.value;
@@ -160,7 +166,7 @@ class Parser {
                 result = Math.min(parseFloat(arg1), parseFloat(arg2));
               }
               break;
-            case "mod":
+            case 'mod':
               {
                 const arg1 = args[0]?.value;
                 const arg2 = args[1]?.value;
@@ -168,7 +174,7 @@ class Parser {
                 result = parseFloat(arg1) % parseFloat(arg2);
               }
               break;
-            case "fac":
+            case 'fac':
               {
                 const arg = args[0]?.value;
                 if (!arg) break;
@@ -192,13 +198,23 @@ class Parser {
           parsed.splice(
             idx - argCount,
             argCount + 1,
-            new Token("Literal", result.toString())
+            new Token('Literal', result.toString())
           );
           break;
         }
       }
+      if (i > 500) throw new Error('Too many iterations.');
+      i++;
     } while (parsed.length > 1);
-    return parsed[0]?.value;
+    if (parsed.length > 1 || parsed.length === 0 || !parsed[0]) {
+      throw new Error('Invalid syntax.');
+    }
+    const parsedResult = parseFloat(parsed[0].value);
+    let finalResult = parsedResult;
+    if (parsedResult % 1 !== 0) {
+      finalResult = parseFloat(finalResult.toFixed(options.decimals));
+    }
+    return finalResult;
   };
 
   parse = (expression: string) => {
@@ -213,29 +229,29 @@ class Parser {
       const { type, value } = t;
 
       switch (type) {
-        case "Literal":
-        case "Var":
+        case 'Literal':
+        case 'Var':
           outQueue.push(t);
           break;
-        case "Function":
-        case "LParen":
+        case 'Function':
+        case 'LParen':
           opStack.push(t);
           break;
-        case "ArgumentSeparator":
-          while (opStack[opStack.length - 1]?.type !== "LParen") {
+        case 'ArgumentSeparator':
+          while (opStack[opStack.length - 1]?.type !== 'LParen') {
             outQueue.push(opStack.pop());
           }
           break;
-        case "Operator":
+        case 'Operator':
           while (
             opStack[opStack.length - 1] &&
-            opStack[opStack.length - 1].type === "Operator"
+            opStack[opStack.length - 1].type === 'Operator'
           ) {
             const o = opStack[opStack.length - 1];
             const tAssoc = this.OPS[value].assoc;
             const tPrec = this.OPS[value].prec;
             const oPrec = this.OPS[o.value].prec;
-            if (tPrec < oPrec || (tAssoc === "left" && tPrec === oPrec)) {
+            if (tPrec < oPrec || (tAssoc === 'left' && tPrec === oPrec)) {
               outQueue.push(opStack.pop());
             } else {
               break;
@@ -243,16 +259,16 @@ class Parser {
           }
           opStack.push(t);
           break;
-        case "RParen":
+        case 'RParen':
           while (
             opStack[opStack.length - 1] &&
-            opStack[opStack.length - 1].type !== "LParen"
+            opStack[opStack.length - 1].type !== 'LParen'
           ) {
             outQueue.push(opStack.pop());
           }
           opStack.pop();
           const lastOp = opStack[opStack.length - 1];
-          if (lastOp && lastOp.type === "Function") {
+          if (lastOp && lastOp.type === 'Function') {
             outQueue.push(opStack.pop());
           }
           break;
@@ -263,7 +279,7 @@ class Parser {
     while (opStack.length > 0) {
       const op = opStack.pop();
       if (!op || /^(LParen|RParen)$/.test(op.type)) {
-        throw new Error("Invalid syntax.");
+        throw new Error('Invalid syntax.');
       }
       outQueue.push(op);
     }
@@ -273,7 +289,7 @@ class Parser {
   parseToString = (expression: string) => {
     const out = this.parse(expression);
     if (!out) return null;
-    const output = out.map((o) => o?.value).join("");
+    const output = out.map((o) => o?.value).join('');
     return output;
   };
 }
